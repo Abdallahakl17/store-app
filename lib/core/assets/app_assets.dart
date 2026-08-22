7395a92c4979924669ab3cde3a0609850cf84d0c
Abdallah akl <dalla.dd123@yahoo.com>
2026-08-22 08:38:41 +0300

    chore:  add assets

--- lib/core/assets/app_assets.dart
+++ lib/core/assets/app_assets.dart
@@ -4,6 +4,10 @@ class AppAssets {
   // Images
   static const String imagesPath = 'assets/images/';
   static const String route = '${imagesPath}splash.png';
+  static const String checkEmail = '${imagesPath}check_email.png';
+  static const String forgetPassword = '${imagesPath}forget_password.png';
+  static const String resetPassword = '${imagesPath}reset_password.png';
+  static const String resetSuccessful = '${imagesPath}reset_successful.png';
 
   // Icons
   static const String iconsPath = 'assets/icons/';
--- lib/splash.dart
+++ lib/splash.dart
@@ -21,7 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     if (!mounted) return;
 
-    context.pushReplacementNamed(AppRoutes.registerView);
+    context.pushReplacementNamed(AppRoutes.loginView);
   }
 
   @override
